8ec3dad46cf8365b2001eb1e05d4878a85096314
PrairieWaltz <85768337+PrairieWaltz@users.noreply.github.com>
2022-10-09 19:01:24 -0400

    responsive_mobile

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-"use strict";
+'use strict';
+
+let menu = document.getElementById('bars');
+let item = document.getElementById('item');
+
+item.style.right = '-300px';
+menu.onclick = function () {
+  if (item.style.right == '-300px') {
+    item.style.right = '0px';
+  } else {
+    item.style.right = '-300px';
+  }
+};
